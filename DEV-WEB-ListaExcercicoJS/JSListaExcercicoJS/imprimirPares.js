aa802c3f9fe00c176e165900b2a2cc434e773910
Melissa Bernardes Goulart <91767583+MelissaBG@users.noreply.github.com>
2022-06-02 09:38:19 -0300

    Add files via upload

---
+++ DEV-WEB-ListaExcercicoJS/JSListaExcercicoJS/imprimirPares.js
@@ -0,0 +1,12 @@
+
+function imprimirPares(){
+
+    
+    const pares = [];
+    
+    for ( const index=0; index < pares.length; index += 2){
+        pares.push(index)
+        }
+        
+    return pares;
+}
---
+++ DEV-WEB-ListaExcercicoJS/JSListaExcercicoJS/maiorNumero.js
@@ -0,0 +1,21 @@
+function maiorNumero(){
+    console.log("Executado...")
+
+    const input_01_value = Number(document.getElementById("input_01").value)
+    const input_02_value = Number(document.getElementById("input_02").value)
+    const input_03_value = Number(document.getElementById("input_03").value)
+    const input_04_value = Number(document.getElementById("input_04").value)
+    const input_05_value = Number(document.getElementById("input_05").value)
+    const input_06_value = Number(document.getElementById("input_06").value)
+    const input_07_value = Number(document.getElementById("input_07").value)
+    const input_08_value = Number(document.getElementById("input_08").value)
+    const input_09_value = Number(document.getElementById("input_09").value)
+    const input_10_value = Number(document.getElementById("input_10").value)
+
+    const array = [input_01_value, input_02_value, input_03_value, input_04_value, input_05_value, input_06_value, input_07_value, input_08_value, input_09_value, input_10_value]
+
+    const Resultado = Math.max(array)
+
+    console.log(Resultado)
+    
+}
